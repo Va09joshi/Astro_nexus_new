bf2c62e00e5b5d31934b5027a03d5d522259e8d5
vaibhav joshi <vaibhavjosh0709@gmail.com>
2026-01-19 15:21:33 +0530

    Chnage Controller

--- models/shop/Product.model.js
+++ models/shop/Product.model.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     description: { type: String },
     price: { type: Number, required: true },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category",default: null, },
     images: [String],
     astrologyType: {
       type: String,
