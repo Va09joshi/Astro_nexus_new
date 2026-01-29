b4528429314db6ee9398520ccdde3a57cca672f9
vaibhav joshi <vaibhavjosh0709@gmail.com>
2026-01-29 18:36:16 +0530

    change Route order

--- controllers/admin/admin.order.controller.js
+++ controllers/admin/admin.order.controller.js
@@ -1,16 +1,11 @@
-const Order = require("../../models/shop/Order.model");
 
-exports.getAllOrders = async (req, res) => {
+import Order from "../../models/shop/Order.model.js";
+
+export const getAllOrders = async (req, res) => {
   try {
     const orders = await Order.find()
-      .populate({
-        path: "items.product",
-        select: "name price images",
-      })
-      .populate({
-        path: "user",
-        select: "name email",
-      })
+      .populate({ path: "items.product", select: "name price images" })
+      .populate({ path: "user", select: "name email" })
       .sort({ createdAt: -1 });
 
     res.json({ success: true, count: orders.length, orders });
@@ -20,18 +15,15 @@ exports.getAllOrders = async (req, res) => {
   }
 };
 
-exports.updateStatus = async (req, res) => {
+export const updateStatus = async (req, res) => {
   try {
     const { id } = req.params;
     const { status } = req.body;
 
     if (!id || !status) return res.status(400).json({ message: "ID and status required" });
 
-    const updatedOrder = await Order.findByIdAndUpdate(
-      id,
-      { status },
-      { new: true }
-    ).populate("items.product", "name price images");
+    const updatedOrder = await Order.findByIdAndUpdate(id, { status }, { new: true })
+      .populate("items.product", "name price images");
 
     if (!updatedOrder) return res.status(404).json({ message: "Order not found" });
 
