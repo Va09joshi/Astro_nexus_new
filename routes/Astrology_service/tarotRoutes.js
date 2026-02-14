8b10e593a8f2a66c3d1c31b48aeacd2cb9d4398e
vaibhav joshi <vaibhavjosh0709@gmail.com>
2026-02-14 16:31:32 +0530

    tarot one feature flag

--- routes/Astrology_service/tarotRoutes.js
+++ routes/Astrology_service/tarotRoutes.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getRandomCards } = require('../../controllers/services/tarotController.js');
+
+const { getRandomCards } = require("../../controllers/services/tarotController.js");
+const { checkFeatureEnabled } = require("../../middlewares/checkFeature.js");
+const { trackFeatureUsage } = require("../../middlewares/trackUsage.js");
+const { authenticateToken } = require("../../middlewares/auth.js");
 
 // GET /api/tarot/random?n=3
-router.get('/random', getRandomCards);
+router.get(
+  "/random",
+  authenticateToken,               // 👈 user must be logged in (optional if you want public)
+  checkFeatureEnabled("tarot"),     // 👈 feature flag
+  trackFeatureUsage,               // 👈 usage tracking
+  getRandomCards
+);
 
 module.exports = router;
